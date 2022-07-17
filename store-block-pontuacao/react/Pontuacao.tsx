47b573c8dd2c7305dc4f3ca031c6c71e4cd834e8
willyoliv <willyoliversilva@gmail.com>
2022-07-17 19:18:36 -0300

    adicionando a tela da carteira de pontos e alterando imagens

--- store-block-pontuacao/react/Pontuacao.tsx
+++ store-block-pontuacao/react/Pontuacao.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useRenderSession } from 'vtex.session-client'
+import { useCssHandles } from 'vtex.css-handles'
 
 interface PontucaoProps {}
 
+const CSS_HANDLES = ['pontos']
+
 const Pontucao: StorefrontFunctionComponent<PontucaoProps> = ({}) => {
   const { loading, session, error } = useRenderSession()
+  const handles = useCssHandles(CSS_HANDLES)
 
   if (loading) {
     return <>Session is loading</>
@@ -13,12 +17,12 @@ const Pontucao: StorefrontFunctionComponent<PontucaoProps> = ({}) => {
   if (error) {
     return <>Session has errors</>
   }
-
+  const pontuacao = 10
   console.log({ session })
 
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className={`${handles.pontos}`}>
+      <h1> Seu saldo é { pontuacao }</h1>
     </div>
   )
 }
